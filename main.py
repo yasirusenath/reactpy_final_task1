1344db0246d1db555e6374a8bee75c2e42a18226
yasirusenath <yasirusenath999@gmail.com>
2023-08-22 13:20:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,95 @@
-ECHO is on.
+# Import necessary modules
+from fastapi import FastAPI
+from reactpy.backend.fastapi import configure
+from reactpy import component, event, html, use_state
+import reactpy as rp
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+
+import uvicorn
+
+import main
+
+
+@component
+def MyCrud():
+    ## Creating state
+    alltodo = use_state([])
+    name, set_name = use_state("")
+    password, set_password = use_state(0)
+
+    def mysubmit(event):
+        newtodo = {"name": name, "password": password}
+
+        # push this to alltodo
+        alltodo.set_value(alltodo.value + [newtodo])
+        login(newtodo)  # function call to login function using the submitted data
+
+    # looping data from alltodo to show on web
+
+    list = [
+        html.li(
+            {
+
+            },
+            f"{b} => {i['name']} ; {i['password']} ",
+        )
+        for b, i in enumerate(alltodo.value)
+    ]
+
+    def handle_event(event):
+        print(event)
+
+    return html.div(
+        {"style": {"padding": "10px"}},
+        ## creating form for submission\
+        html.form(
+            {"on submit": mysubmit},
+            html.h1("Login Form - ReactPy & Mongodb"),
+            html.input(
+                {
+                    "type": "test",
+                    "placeholder": "Name",
+                    "on_change": lambda event: set_name(event["target"]["value"]),
+                }
+            ),
+            html.input(
+                {
+                    "type": "test",
+                    "placeholder": "Password",
+                    "on_change": lambda event: set_password(event["target"]["value"]),
+                }
+            ),
+            # creating submit button on form
+            html.button(
+                {
+                    "type": "submit",
+                    "on_click": event(
+                        lambda event: mysubmit(event), prevent_default=True
+                    ),
+                },
+                "Submit",
+            ),
+        ),
+        html.ul(list),
+    )
+
+
+app = FastAPI()
+from pymongo import MongoClient
+
+uri = "mongodb+srv://yasiru11:yasiru11@cluster1.otlcmjt.mongodb.net/test?retryWrites=true&w=majority"
+client = MongoClient(uri)
+
+# Defining the database name
+db = client['reactpy_1']
+
+# Defining the collection name
+collection = db['reactpy001']
+
+# Checking the connection
+try:
+    client.admin.command("ping")
+    print("Successfully connected to MongoDB")
+except Exception as e:
+    print()
